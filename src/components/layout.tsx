ee898082f9d499fa99af02d15da2f9b8b5902581
Shawn Bowler <grimtrisharalok@gmail.com>
2023-05-19 05:02:55 +0800

    Hide theme toggle on smaller displays

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -7,7 +7,7 @@ export const PageLayout = (props: PropsWithChildren) => {
       <div className="h-full w-full overflow-y-scroll border-x border-slate-500 md:max-w-4xl">
         {props.children}
       </div>
-      <div className="absolute right-4 top-4 ">
+      <div className="absolute right-4 top-4 hidden lg:block">
         <ThemeToggle />
       </div>
     </main>
